ebd1c67fb96c3622ed1610289d395e49185246a6
Krystian994 <63874373+Krystian994@users.noreply.github.com>
2021-10-31 18:07:11 +0100

    Add files via upload

---
+++ PHP3/app/security/login_view.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<title>Logowanie</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="{$app_url}/css/main.css" />
+                <link rel="stylesheet" href="{$app_url}/css/style.css">
+		<noscript><link rel="stylesheet" href="{$app_url}/css/noscript.css" /></noscript>
+</head>
+<body>
+
+<div style="width:90%; margin: 2em auto;">
+
+<form action="<?php print(_APP_ROOT); ?>/app/security/login.php" method="post" class="pure-form pure-form-stacked">
+	<legend>Logowanie</legend>
+	<fieldset>
+		<label for="id_login">login: </label>
+		<input id="id_login" type="text" name="login" value="<?php out($form['login']); ?>" />
+		<label for="id_pass">pass: </label>
+		<input id="id_pass" type="password" name="pass" />
+	</fieldset>
+	<input type="submit" value="zaloguj" class="pure-button pure-button-primary"/>
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+</div>
+    <!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.scrolly.min.js"></script>
+			<script src="js/jquery.dropotron.min.js"></script>
+			<script src="js/jquery.scrollex.min.js"></script>
+			<script src="js/browser.min.js"></script>
+			<script src="js/breakpoints.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+</body>
+</html>
